29658878343a8309c5909ef5e212eed4c6f08f0c
Alex Dennis <alex.dennis@gmail.com>
2015-05-30 17:11:44 -0400

    Listening artist to search

---
+++ app/src/main/java/com/carltondennis/spotifystreamer/ArtistsAdapter.java
@@ -0,0 +1,49 @@
+package com.carltondennis.spotifystreamer;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by alex on 5/30/15.
+ */
+public class ArtistsAdapter extends CursorAdapter {
+
+    public ArtistsAdapter(Context context, Cursor c, int flags) {
+
+        super(context, c, flags);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        View view = LayoutInflater.from(context).inflate(R.layout.list_item_artist, parent, false);
+        view.setTag(new ViewHolder(view));
+        return view;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+        ViewHolder viewHolder = (ViewHolder) view.getTag();
+
+        // TODO set image and name
+    }
+
+    /**
+     * Cache of the children views for a forecast list item.
+     */
+    public static class ViewHolder {
+        public final ImageView imageView;
+        public final TextView nameView;
+
+        public ViewHolder(View view) {
+            imageView = (ImageView) view.findViewById(R.id.list_item_image_artist);
+            nameView = (TextView) view.findViewById(R.id.list_item_name_artist);
+        }
+    }
+}
--- app/src/main/java/com/carltondennis/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/com/carltondennis/spotifystreamer/MainActivityFragment.java
@@ -2,9 +2,14 @@ package com.carltondennis.spotifystreamer;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ListView;
 
 
 /**
@@ -12,12 +17,43 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
+    private static final String TAG = MainActivityFragment.class.getCanonicalName();
+
+    private ArtistsAdapter mArtistsAdapter;
+    private ListView mArtistsList;
+    private EditText mArtistSearchBox;
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_main, container, false);
+
+        mArtistsAdapter = new ArtistsAdapter(getActivity(), null, 0);
+        mArtistsList = (ListView) rootView.findViewById(R.id.artists_list);
+        mArtistsList.setAdapter(mArtistsAdapter);
+        mArtistSearchBox = (EditText) rootView.findViewById(R.id.search_artists);
+        mArtistSearchBox.addTextChangedListener(new ArtistSearchTextWatcher());
+
+        return rootView;
+    }
+
+    class ArtistSearchTextWatcher implements TextWatcher {
+
+        @Override
+        public void afterTextChanged(Editable s) {}
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String text = mArtistSearchBox.getText().toString().trim();
+            if (text.length() > 2) {
+                Log.d(TAG, text);
+            }
+        }
     }
 }
